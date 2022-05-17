2d9d178cd5046299682e2a862dfcc6c2bd95c6bd
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-17 11:23:16 -0300

    Eventos Onclick e Change

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let btn = document.getElementById('clic');
+//Evento click
+
+/* let btn = document.getElementById('clic');
 btn.addEventListener('click', function () {
     alert('você clicou');
+}) */
+
+var linguagens = document.getElementById('linguagens');
+linguagens.addEventListener('change', function () {
+    var options = this.options;
+    var itemAtual = this.selectedIndex;
+    console.log(options[itemAtual].text);
 })
